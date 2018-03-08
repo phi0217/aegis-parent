9f79e61c9a358e2058b8cb10ff1e22ccf0779b54
xiefei <xiefei@tan66.com>
2018-03-08 12:07:06 +0800

    修改结构

--- aegis-authorize/src/main/java/com/vxianjin/aegis/authorize/service/BackUserService.java
+++ aegis-authorize/src/main/java/com/vxianjin/aegis/authorize/service/BackUserService.java
@@ -1,49 +1,8 @@
-/**
- * 
- */
 package com.vxianjin.aegis.authorize.service;
 
-import com.vxianjin.aegis.authorize.dto.AdminCondition;
-import com.vxianjin.aegis.authorize.dto.AdminInfo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 /**
- * 管理员服务
- * 
- * @author zhailiang
- *
+ * @author xiefei
+ * @date 2018/03/08
  */
 public interface BackUserService {
-
-//	/**
-//	 * 创建管理员
-//	 * @param adminInfo
-//	 * @return
-//	 */
-//	AdminInfo create(AdminInfo adminInfo);
-//	/**
-//	 * 修改管理员
-//	 * @param adminInfo
-//	 * @return
-//	 */
-//	AdminInfo update(AdminInfo adminInfo);
-//	/**
-//	 * 删除管理员
-//	 * @param id
-//	 */
-//	void delete(Long id);
-//	/**
-//	 * 获取管理员详细信息
-//	 * @param id
-//	 * @return
-//	 */
-//	AdminInfo getInfo(Long id);
-//	/**
-//	 * 分页查询管理员
-//	 * @param condition
-//	 * @return
-//	 */
-//	Page<AdminInfo> query(AdminCondition condition, Pageable pageable);
-
 }
